65928e63f613f387fa78fd454aa27c5b1863d679
Martijn Bentum <bentummartijn@gmail.com>
2020-07-22 14:03:02 +0200

    added some fixtures for simple models, cleaned up code

--- utils/make_keywords_types.py
+++ utils/make_keywords_types.py
@@ -1,36 +1,52 @@
 from django.apps import apps
+from django import db
 import pandas as pd
 from misc.models import Keyword, KeywordRelation
+from sources.models import Available, RequestUsePermission
+
+def save_model(instance,saved,already_exists,error):
+	ok = False
+	try: instance.save()
+	except db.IntegrityError: already_exists.append(instance)
+	except: error.append(instance)
+	else: 
+		saved.append(instance)
+		ok =True
+	return ok
+	
+
+
+def show_results(model_name,saved,already_exists,error):
+	print(model_name+':\n'+'-'*(len(model_name)+1))
+	print('saved:',len(saved),'already exists:',len(already_exists),'error:',len(error))
+	print()
 
 d = pd.read_excel('data/keywords_and_types.xlsx',sheet_name=None)
 
 def make_keywords():
 	kw = d['Keywords']
 	kw = [list(line) for line in kw.values[4:,1:3]]
+	saved,already_exists,error = [],[],[]
+	rsaved,ralready_exists,rerror = [],[],[]
 	for category,name in kw:
-		new_name_keyword = False
-		try:
-			category_keyword = Keyword(name = category)
-			category_keyword.save()
-			ck = category_keyword
-			print('category:',category,'saved')
-		except: print('category:',category,'already exists')
-		try:
-			name_keyword = Keyword(name = name)
-			name_keyword.save()
-			new_name_keyword = True
-			print('category:',category,'name:',name,'saved')
-		except: print('category:',category,'name:',name,'already exists')
-		if new_name_keyword:
-			try:
-				kr = KeywordRelation(container=ck,contained=name_keyword)
-				kr.save()
-				print(kr,'saved')
-			except: print('relation already exists:',category,name)
+		category_keyword = Keyword(name = category)
+		name_keyword = Keyword(name = name)
+		okck = save_model(category_keyword,saved,already_exists,error)
+		oknk =save_model(name_keyword,saved,already_exists,error)
+		if okck: ck = category_keyword
+		else: ck = Keyword.objects.get(name =category)
+		if oknk: nk = name_keyword
+		else: nk = Keyword.objects.get(name= name)
+		kr = KeywordRelation(container=ck,contained=nk)
+		save_model(kr,rsaved,ralready_exists,rerror)
+
+	show_results('Keyword',saved,already_exists,error)
+	show_results('KeywordRelation',rsaved,ralready_exists,rerror)
 			
 
 
 def make_types():
+	saved,already_exists,error = [],[],[]
 	for key in d.keys():
 		if not 'types' in key: continue
 		model_name = key.split(' ')[0]+'Type'
@@ -38,14 +54,35 @@ def make_types():
 		names = d[key].values
 		for name in names:
 			name = name[0]
-			try:
-				instance = model(name = name)
-				instance.save()
-				print(model_name,name,'saved')
-			except:
-				print(model_name,name,'alread exists')
+			instance = model(name = name)
+			save_model(instance,saved,already_exists,error)
+		show_results(model_name,saved,already_exists,error)
+
+
+def make_simple_model_instance(app_name,model_name,names):
+	model = apps.get_model(app_name,model_name)
+	o,saved,already_exists,error = [],[],[],[]
+	for name in names.split(','):
+		instance = model(name = name) 
+		o.append(instance)
+		save_model(instance,saved,already_exists,error)
+	show_results(model_name,saved,already_exists,error)
+	return o
+
+def make_simple_models():
+	available = 'sources','Available','available on site,available through link,unavailable'
+	gender = 'persons','Gender','male,female,unknown,other'
+	request_use_permission = 'sources','RequestUsePermission','yes,no'
+	target_audience = 'sources','TargetAudience','national,internation,both'
+	rated = 'sources','Rated','general,<14,14+'
+	o = []
+	for sm in [available,gender,request_use_permission,target_audience,rated]:
+		o.extend(make_simple_model_instance(*sm))
+	return o
+	
 
 def make():
 	make_keywords()
 	make_types()
+	make_simple_models()
 
