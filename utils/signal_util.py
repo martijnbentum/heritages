4e5c6125518761870c961de7b3796c18aa30727d
Martijn Bentum <bentummartijn@gmail.com>
2020-07-24 17:49:45 +0200

    code to catch m2m changes and save them to the easyautit events

---
+++ utils/signal_util.py
@@ -0,0 +1,58 @@
+from django.apps import apps
+import sys
+from easyaudit.models import CRUDEvent
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+from misc.models import Famine
+from sources.models import Film,Text,Image,PictureStory,Music,Infographic
+from persons.models import Person
+
+def catch_m2m(instance, action, pk_set, model_name,field_name):
+	z = False
+	field = getattr(instance,field_name)
+	if action == 'pre_remove':
+		before = field.all()
+		after = [n for n in field.all() if n.pk not in pk_set]
+		z = True
+	elif action == 'post_add':
+		after= field.all()
+		before= [n for n in field.all() if n.pk not in pk_set]
+		z = True
+	if z:
+		event = CRUDEvent.objects.filter(content_type__model = model_name,object_id= instance.pk)[0]
+		v = {field_name:[','.join([str(n) for n in before]),','.join([str(n) for n in after])]}
+		if type(event.changed_fields) != dict: event.changed_fields = v
+		else: 
+			event.changed_fields.update(v)
+		event.save()
+		print(model_name,field_name, 'before:',','.join([str(n) for n in before]))
+		print(model_name,field_name,'after:',','.join([str(n) for n in after]))
+	
+'''
+@receiver(m2m_changed,sender=Famine.names.through)
+def bla(sender, instance,action,**kwargs):
+	pk_set = kwargs['pk_set']
+	catch_m2m(instance,action,pk_set,'famine','names')
+'''
+
+def make_m2mreceiver(sender,model_name,field_name):
+	m = '@receiver(m2m_changed, sender='+sender+')\n'
+	m+= 'def '+sender.replace('.','__').lower() +'(sender,instance,action,**kwargs):\n'
+	m+= '\tpk_set = kwargs["pk_set"]\n'
+	m+= '\tcatch_m2m(instance,action,pk_set,"'+model_name+'","'+field_name+'")'
+	exec(m,globals())
+
+def model2m2mreceiver(model_name,app_name):
+	model = apps.get_model(app_name,model_name)
+	m2m_fieldnames = model._meta.__dict__['local_many_to_many']
+	for f in m2m_fieldnames:
+		field_name = f.attname
+		sender = model_name+'.'+field_name+'.through'
+		make_m2mreceiver(sender,model_name.lower(),field_name)
+
+names = 'Famine$misc,Film$sources,Text$sources,Image$sources,PictureStory$sources'
+names += ',Music$sources,Infographic$sources,Person$persons'
+for name in names.split(','):
+	model_name, app_name = name.split('$')
+	model2m2mreceiver(model_name,app_name)
