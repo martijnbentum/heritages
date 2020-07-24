78bdd4ca05db1b7cfe86e9cfd666ca9e2a788ae8
Martijn Bentum <bentummartijn@gmail.com>
2020-07-24 17:45:07 +0200

    easyaudit middle to setting to connect to current user

--- heritages/settings.py
+++ heritages/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
 ]
 
 ROOT_URLCONF = 'heritages.urls'
