f32f791f48f0cbeddbcaa67455ea27d314067839
Martijn Bentum <bentummartijn@gmail.com>
2020-08-14 15:07:52 +0200

    media folders set

--- heritages/urls.py
+++ heritages/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,5 @@ urlpatterns = [
 	path('utilities/',include('utilities.urls')),
 	re_path(r'^select2/',include('django_select2.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
