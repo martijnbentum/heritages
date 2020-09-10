1177f2cf85f4f15b081a85a8316ee8e275775f89
Martijn Bentum <bentummartijn@gmail.com>
2020-09-10 11:56:31 +0200

    created a tag to check whether user is part of a group

--- utilities/templatetags/getattribute.py
+++ utilities/templatetags/getattribute.py
@@ -13,5 +13,9 @@ def get_value_from_dict(dictionary,key):
 		return dictionary[key]
 	return settings.TEMPLATE_STRING_IF_INVALID
 
+def has_group(user,group_name):
+	return user.groups.filter(name=group_name).exists()
+
 register.filter('getattribute',getattribute)
 register.filter('get_value_from_dict',get_value_from_dict)
+register.filter('has_group',has_group)
