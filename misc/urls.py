8647b253cb0f8675d11e3596052eddfa647b0b58
Martijn Bentum <bentummartijn@gmail.com>
2021-02-18 17:37:46 +0100

    #52 added keyword urls

--- misc/urls.py
+++ misc/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 	path('add_famine/<str:view>',views.edit_famine, name='add_famine'),
 	path('edit_famine/<int:pk>/',views.edit_famine, name='edit_famine'),
 	path('edit_famine/<int:pk>/<str:focus>/',views.edit_famine, name='edit_famine'),
+	path('edit_keyword/<int:pk>/',views.edit_keyword, name='edit_keyword'),
+	path('edit_keyword/<int:pk>/<str:focus>/',views.edit_keyword, name='edit_keyword'),
 	path('add_faminename/',views.add_faminename, name='add_faminename'),
 	path('add_faminename/<int:pk>',views.add_faminename, name='add_faminename'),
 	path('add_causaltrigger/',views.add_causaltrigger, name='add_causaltrigger'),
@@ -19,7 +21,8 @@ urlpatterns = [
 	path('add_causal_trigger/<int:pk>',views.add_causaltrigger, name='add_causal_trigger'),
 	path('add_language/',views.add_language, name='add_language'),
 	path('add_language/<int:pk>',views.add_language, name='add_language'),
-	path('add_keyword/',views.add_keyword, name='add_keyword'),
-	path('add_keyword/<int:pk>',views.add_keyword, name='add_keyword'),
+	path('add_keyword/',views.edit_keyword, name='add_keyword'),
+	path('add_keyword/<str:view>',views.edit_keyword, name='add_keyword'),
 	path('delete/<int:pk>/<str:model_name>',views.delete,name='delete'),
 ]
+
