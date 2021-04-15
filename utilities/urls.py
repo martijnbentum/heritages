2353e1da39cf5d1fe59395a6909a2cb48801b625
Martijn Bentum <bentummartijn@gmail.com>
2021-04-15 14:11:52 +0200

    added row view url

--- utilities/urls.py
+++ utilities/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 	path('list_view/<str:model_name>/<str:app_name>/<int:max_entries>/',views.list_view,name='list_view'),
 	path('list_view/<str:model_name>/<str:app_name>/<str:html_name>/',views.list_view,name='list_view'),
 	path('list_view_general/<str:model_name>/<str:app_name>/<str:field_names>/',views.list_view,name='list_view_general'),
+	path('row_view/<str:model_name>/<str:app_name>/',views.row_view,name='row_view'),
 ]
