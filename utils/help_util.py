9a4ce6d4d845494f36d5a190efeb56e9848f9da9
Martijn Bentum <bentummartijn@gmail.com>
2021-06-17 14:20:38 +0200

    added function load_protocol_in_database to save all protocols in the Protocol excel file in the database

--- utils/help_util.py
+++ utils/help_util.py
@@ -1,5 +1,6 @@
 import json
 from openpyxl import load_workbook
+from utilities.models import Protocol
 
 
 class Helper:
@@ -30,6 +31,7 @@ class Helper:
 
 class ModelHelper:
 	def __init__(self,wb,name):
+		self.protocols = Protocol.objects.filter(model_name=name.lower())
 		self.wb = wb
 		self.name = name
 		self.sheet = wb[name]
@@ -47,9 +49,15 @@ class ModelHelper:
 				
 	def _make_dict(self):
 		d = {}
-		for fh in self.field_helpers.values():
-			d[fh.field_name] =fh.help_text
-			d[fh.field_name.lower().strip()] =fh.help_text_html
+		if self.protocols:
+			print('creating help text with protocols')
+			for p in self.protocols:
+				d[p.field_name] =  p.explanation 
+				d[p.field_name.lower().strip()] = '<p>' + p.explanation + '</p>'
+		else:
+			for fh in self.field_helpers.values():
+				d[fh.field_name] =fh.help_text
+				d[fh.field_name.lower().strip()] =fh.help_text_html
 		return d
 			
 		
@@ -77,6 +85,42 @@ class FieldHelper:
 		m +='\n' + self.help_text
 		return m
 	
+names = 'Image,Infographic,Film,Music,PictureStory,Text,Videogame,Recordedspeech'
+names += ',Memorialsite,Artefact'
+other_names = 'Famine,Location,Person,Keyword'.split(',')
+app_names  = 'misc,locations,persons,misc'.split(',')
 
+names = ','.join(['sources|'+n for n in names.split(',')]) +','
+names += ','.join([app_names[i] +'|' +n for i,n in enumerate(other_names)])
+
+def load_protocol_in_database(save= False):
+	h = Helper()
+	protocols = []
+	for name in names.split(','):
+		print(name)
+		app_name, model_name = name.split('|')
+		d = h.get_dict(model_name)
+		for field_name in d.keys():
+			if '<p>' in d[field_name]: continue
+			explanation = d[field_name]
+			p = _make_protocol(app_name,model_name,field_name,explanation,save)
+			protocols.append(p)
+	print('done')
+	return protocols
+
+def _make_protocol(app_name,model_name,field_name,explanation,save=False):
+	app_name, model_name = app_name.lower(), model_name.lower()
+	try:p = Protocol.objects.get(app_name=app_name,model_name=model_name,field_name=field_name)
+	except: 
+		print('making new protocol:',model_name,field_name,'\n',explanation,'\n---\n')
+		p = Protocol(app_name=app_name,model_name=model_name,
+			field_name=field_name, explanation= explanation)
+	else:
+		print('setting explanation (protocol exists):',p.explanation,'\nwith\n',explanation,'\n---\n')
+		p.explanation = explanation
+	if save:p.save()
+	return p
+	
+		
 	
 		
