31f97b502c753723f533dbc92498485a7f942ebc
Martijn Bentum <bentummartijn@gmail.com>
2022-02-01 11:43:58 +0100

    add a function to fix all diacritic filenames; add a function to fix filenames if the new name already exists

--- utils/cleanup_filenames.py
+++ utils/cleanup_filenames.py
@@ -1,12 +1,14 @@
 import os
 import dcl
+from django.apps import apps
+from .model_util import make_models_image_file_dict, id_generator
 
 def rename_file(instance,file_fieldname,new_name, overwrite = False):
 	file_field = getattr(instance,file_fieldname)
 	old_path = file_field.path
 	base_dir = old_path.replace(file_field.name,'')
 	new_path = base_dir + new_name
-	if not overwrite: assert not os.path.isfile(new_path)
+	if not overwrite and os.path.isfile(new_path): new_path = _fix(new_path)
 	os.rename(old_path,new_path)
 	setattr(instance,file_fieldname,new_name)
 	instance.save()
@@ -16,7 +18,7 @@ def remove_diacritics_filename_existing_file(instance,file_fieldname):
 	if not dcl.has_diacritics(file_field.name):return
 	new_name = dcl.clean_diacritics(file_field.name)
 	rename_file(instance,file_fieldname,new_name)
-	print(new_name,'updated filename\n')
+	print(instance, new_name, 'updated filename\n')
 
 def rename_all_dicritic_illustration_filenames():
 	from catalogue.models import Illustration
@@ -29,12 +31,25 @@ def remove_diacritics_filename(filename):
 	return dcl.clean_diacritics(filename)
 
 
+def _handle_instance(instance,field_names):
+	for field_name in field_names:
+		file_field= getattr(instance,field_name)
+		if file_field: remove_diacritics_filename_existing_file(instance,field_name)
 
-def rename_all_dicritic_publication_filenames():
-	from catalogue.models import Publication
-	i = Publication.objects.all()
-	for x in i:
-		if not x.pdf: pass
-		else:remove_diacritics_filename_existing_file(x,'pdf')
-		if not x.cover: pass
-		else:remove_diacritics_filename_existing_file(x,'cover')
+def rename_all_dicritic_filenames():
+	d = make_models_image_file_dict()
+	for app_model_name, field_names in d.items():
+		model = apps.get_model(*app_model_name)
+		instances = model.objects.all()
+		for instance in instances:
+			_handle_instance(instance,field_names)
+
+def _fix(filename):
+	temp = filename.split('.')
+	if len(temp) == 1: return temp[0] + '_django-' + id_generator()
+	extension = temp[-1]
+	path = '-'.join(temp[:-1])
+	path += '_django-' + id_generator()
+	return path + '.' + extension
+
+
