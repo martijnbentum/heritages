d24486fce8cffc2b2d6e9e5223365dd9cc7fee2b
Martijn Bentum <bentummartijn@gmail.com>
2022-09-01 15:17:29 +0200

    added download image view

--- heritages/views.py
+++ heritages/views.py
@@ -7,33 +7,70 @@ from django.contrib.auth.decorators import login_required, permission_required
 
 @permission_required('utilities.view_generic')
 def protected_media(request, filename):
-	print(filename,'filename')
-	"""
-	View to send file via X-Sendfile
-	:param request: the HTTP request
-	:param filename: the filename, extracted from the url
-	:return: a HttpResponse object
-	"""
-	# Construct full path and base name
-	full_path = os.path.join(settings.MEDIA_ROOT, filename)
-	base_name = os.path.basename(filename)
+    print(filename,'filename')
+    """
+    View to send file via X-Sendfile
+    :param request: the HTTP request
+    :param filename: the filename, extracted from the url
+    :return: a HttpResponse object
+    """
+    # Construct full path and base name
+    full_path = os.path.join(settings.MEDIA_ROOT, filename)
+    base_name = os.path.basename(filename)
 
-	# Check whether the file exist
-	if not os.path.exists(full_path):
-		raise Http404(_("The requested file does not exist"))
+    # Check whether the file exist
+    if not os.path.exists(full_path):
+        raise Http404(_("The requested file does not exist"))
 
-	#Let Django serve it if the XSENDFILE setting is false
-	if not settings.XSENDFILE:
-	    from django.views.static import serve
-	    return serve(request, full_path, document_root='/')
+    #Let Django serve it if the XSENDFILE setting is false
+    if not settings.XSENDFILE:
+        from django.views.static import serve
+        return serve(request, full_path, document_root='/')
 
-	# Determine the mime type
-	(mime_type, encoding) = mimetypes.guess_type(full_path)
+    # Determine the mime type
+    (mime_type, encoding) = mimetypes.guess_type(full_path)
 
-	# Construct the response
-	response = HttpResponse(content_type=mime_type)
-	response['Content-Disposition'] = 'inline;filename='+base_name
-	response['X-Sendfile'] = full_path
+    # Construct the response
+    response = HttpResponse(content_type=mime_type)
+    response['Content-Disposition'] = 'inline;filename='+base_name
+    response['X-Sendfile'] = full_path
 
-	return response
+    return response
+
+@permission_required('utilities.view_generic')
+def download_image(request, filename):
+    print(filename,'filename')
+    """
+    View to send file via X-Sendfile
+    :param request: the HTTP request
+    :param filename: the filename, extracted from the url
+    :return: a HttpResponse object
+    """
+    # Construct full path and base name
+    if filename.startswith('/media/'):  
+        print('ok')
+        filename = filename[len('/media/'):]
+        print(filename,'ok')
+    full_path = os.path.join(settings.MEDIA_ROOT, filename)
+    base_name = os.path.basename(filename)
+    print(full_path,'full_path')
+
+    # Check whether the file exist
+    if not os.path.exists(full_path):
+        raise Http404(_("The requested file does not exist"))
+
+    #Let Django serve it if the XSENDFILE setting is false
+    if not settings.XSENDFILE:
+        from django.views.static import serve
+        return serve(request, full_path, document_root='/')
+
+    # Determine the mime type
+    (mime_type, encoding) = mimetypes.guess_type(full_path)
+
+    # Construct the response
+    response = HttpResponse(content_type=mime_type)
+    response['Content-Disposition'] = 'attachment;filename='+base_name
+    response['X-Sendfile'] = full_path
+
+    return response
 
