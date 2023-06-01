f9d5f88c9916e8f7627baebf0cc2da764c16b851
Martijn Bentum <bentummartijn@gmail.com>
2023-06-01 10:29:53 +0200

    AddInfoForm to set info for adding reference and license to items in d

--- utilities/forms.py
+++ utilities/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm, formset_factory
 from django_select2.forms import ModelSelect2Widget
 from .models import Protocol
+from utils import model_util
 
 
 
@@ -25,4 +26,23 @@ class ProtocolForm(ModelForm):
 	class Meta:
 		model = Protocol
 		fields = ['field_name','explanation']
+
+
+class AddInfoForm(forms.Form):
+    model_names = ['all'] + model_util.all_model_names
+    model_options = ([(name,name) for name in model_names])
+    model = forms.ChoiceField(
+        label='all',
+        widget=forms.Select,
+        choices = model_options
+    )
+    check_names = 'reference & license,reference,license'.split(',')
+    check_options = ([(name,name) for name in check_names])
+    check_empty = forms.ChoiceField(
+        label='both',
+        widget=forms.Select,
+        choices = check_options
+    )
+    skip_visited = forms.BooleanField(required = False)
+
 
