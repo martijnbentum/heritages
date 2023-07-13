52b2966a3604c3ed30d76463dbd8801b04e67b6c
martijnbentum <bentummartijn@gmail.com>
2023-07-13 15:17:45 +0200

    changed layout

--- utilities/views.py
+++ utilities/views.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from django.apps import apps
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
 from django.core import serializers
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -95,11 +96,9 @@ def search_view(request, view_type = '', query = ' ', combine = ' ',
 def get_user_search_requests(request):
     directory = 'user_search_requests/' + request.session.session_key+'/'
     if not os.path.isdir(directory): os.mkdir(directory)
-    print('  user req received\n','\033[91m'+time.strftime("%H:%M:%S")+' '+str(time.time()).split('.')[-1]+'\033[0m')
     o = list(request.FILES['file'].chunks())[0].decode('utf-8')
     with open(directory + 'search','w') as fout:
         fout.write(o)
-    print('  file written\n','\033[91m'+time.strftime("%H:%M:%S")+' '+str(time.time()).split('.')[-1]+'\033[0m')
     with open(directory + 'ready', 'w') as fout:
         pass
     return HttpResponse('done', content_type='text/plain')
@@ -124,8 +123,10 @@ def list_view(request, model_name, app_name,html_name='',field_names = '',
     field_dict = _handle_fieldnames(field_names) if field_names else {}
     var = {name +'_list':instances,'page_name':model_name,
         'order':s.order.order_by,'direction':s.order.direction,
-        'query':s.query.query,'nentries':s.nentries, 'list':instances,'name':name,
-        'type_name':name+'_type','app_name':app_name,'fields':field_dict.items(),
+        'query':s.query.query,'nentries':s.nentries, 
+        'list':instances,'name':name,
+        'type_name':name+'_type','app_name':app_name,
+        'fields':field_dict.items(),
         'delete':app_name+':delete','edit':app_name+':edit_'+name,
         'typemaster':typemaster}
     r =  render(request, html_name.replace('$','/'),var)
@@ -135,10 +136,11 @@ def timer(start):
     return time.time() -start
 
 @permission_required('utilities.add_generic')
-def edit_model(request, name_space, model_name, app_name, instance_id = None, 
-    formset_names='', focus='', view ='complete'):
+def edit_model(request, name_space, model_name, app_name,  
+    instance_id = None, formset_names='', focus='', view ='complete'):
     '''edit view generalized over models.
-    assumes a 'add_{{model_name}}.html template and edit_{{model_name}} function
+    assumes a 'add_{{model_name}}.html template 
+    and edit_{{model_name}} function
     and {{model_name}}Form
     '''
     print('focus',[focus])
@@ -156,48 +158,59 @@ def edit_model(request, name_space, model_name, app_name, instance_id = None,
         focus, button = getfocus(request), getbutton(request)
         print('focus',[focus])
         if button in 'delete,cancel,confirm_delete': 
-            return delete_model(request,name_space,model_name,app_name,instance_id)
-        if button == 'saveas' and instance: instance = copy_complete(instance)
+            return delete_model(request,name_space,model_name,
+                app_name,instance_id)
+        if button == 'saveas' and instance: 
+            instance = copy_complete(instance)
         if button == 'skip': return show_edit_screen(request)
         form = modelform(request.POST, request.FILES, instance=instance)
         if form.is_valid():
             print('form is valid: ',form.cleaned_data,type(form))
-            # print(form.cleaned_data['image_file'],str(form.cleaned_data['image_file']))
             instance = form.save()
             if view == 'complete':
-                ffm = FormsetFactoryManager(name_space,names,request,instance)
+                ffm = FormsetFactoryManager(name_space,names,request,
+                    instance)
                 valid = ffm.save()
                 if valid:
                     show_messages(request,button, model_name)
                     if button== 'add_another':
-                        return HttpResponseRedirect(reverse(app_name+':add_'+model_name.lower()))
-                    if parameter_focus == 'add_info': return show_edit_screen(request)
+                        m =app_name+':add_'+model_name.lower()
+                        return HttpResponseRedirect(reverse(m))
+                    if parameter_focus == 'add_info': 
+                        return show_edit_screen(request)
                     return HttpResponseRedirect(reverse(
                         app_name+':edit_'+model_name.lower(), 
                         kwargs={'pk':instance.pk,'focus':focus}))
                 else: print('ERROR',ffm.errors)
             else: return HttpResponseRedirect('/utilities/close/')
         else:
-            print(list(form.non_field_errors()))
-            [show_messages(request,'error',l) for l in list(form.non_field_errors())]
+            nfe = list(form.non_field_errors())
+            print(nfe)
+            [show_messages(request,'error',l) for l in nfe]
     if not form: form = modelform(instance=instance)
-    if not ffm: ffm = FormsetFactoryManager(name_space,names,instance=instance)
+    if not ffm: 
+        ffm = FormsetFactoryManager(name_space,names,instance=instance)
     tabs = make_tabs(model_name.lower(), focus_names = focus)
-    page_name = 'Edit ' +model_name.lower() if instance_id else 'Add ' +model_name.lower()
+    if instance_id: page_name = 'Edit ' + model_name.lower()
+    else: page_name = 'Add ' + model_name.lower()
     helper = help_util.Helper(model_name = model_name)
     args = {'form':form,'page_name':page_name,'crud':crud,
-        'tabs':tabs, 'view':view,'app_name':app_name,'model_name':model_name,
+        'tabs':tabs, 'view':view,'app_name':app_name,
+        'model_name':model_name,
         'helper':helper.get_dict(), 'thumbnail_size':thumbnail_size,
         'focus':focus}
     args.update(ffm.dict)
-    return render(request,app_name+'/add_' + model_name.lower() + '.html',args)
+    url = app_name+'/add_' + model_name.lower() + '.html'
+    return render(request,url,args)
         
 
 @permission_required('utilities.add_generic')
-def add_simple_model(request, name_space,model_name,app_name, page_name, pk= None):
+def add_simple_model(request, name_space,model_name,app_name, 
+    page_name, pk= None):
     '''Function to add simple models with only a form could be extended.
     request     django object
-    name_space  the name space of the module calling this function (to load forms / models)
+    name_space  the name space of the module calling this function 
+                (to load forms / models)
     model_name  name of the model
     app_name    name of the app
     page_name   name of the page
@@ -213,20 +226,24 @@ def add_simple_model(request, name_space,model_name,app_name, page_name, pk= Non
         print(button,'12345667889999')
         if button in 'delete,confirm_delete': 
             print('deleting simple model')
-            return delete_model(request,name_space,model_name,app_name,pk,True)
+            return delete_model(request,name_space,model_name,
+                app_name,pk,True)
         if form.is_valid():
             form.save()
             messages.success(request, model_name + ' saved')
             return HttpResponseRedirect('/utilities/close/')
     if not form: form = modelform(instance=instance)
     instances = model.objects.all().order_by('name')
-    page_name = 'Edit ' +model_name.lower() if pk else 'Add ' +model_name.lower()
+    if pk: page_name = 'Edit ' +model_name.lower()
+    else: page_name = 'Add ' +model_name.lower()
     url = '/'.join(request.path.split('/')[:-1])+'/' if pk else request.path
-    var = {'form':form, 'page_name':page_name, 'instances':instances,'url':url}
+    var = {'form':form, 'page_name':page_name, 'instances':instances,
+        'url':url}
     return render(request, 'utilities/add_simple_model.html',var)
 
 @permission_required('utilities.delete_generic')
-def delete_model(request, name_space, model_name, app_name, pk, close = False):
+def delete_model(request, name_space, model_name, app_name, pk, 
+    close = False):
     model = apps.get_model(app_name,model_name)
     instance= get_object_or_404(model,id =pk)
     focus, button = getfocus(request), getbutton(request)
@@ -243,7 +260,8 @@ def delete_model(request, name_space, model_name, app_name, pk, close = False):
             instance.delete()
             show_messages(request,button, model_name)
             if close: return HttpResponseRedirect('/utilities/close/')
-            return HttpResponseRedirect('/utilities/list_view/'+model_name.lower()+'/'+app_name+'/')
+            url='/utilities/list_view/'+model_name.lower()+'/'+app_name+'/'
+            return HttpResponseRedirect(url)
     info = instance.info
     print(1,info,instance,pk)
     var = {'info':info,'page_name':'Delete '+model_name.lower()}
@@ -252,10 +270,12 @@ def delete_model(request, name_space, model_name, app_name, pk, close = False):
     
 
 def getfocus(request):
-    '''extracts focus variable from the request object to correctly set the active tabs.'''
+    '''extracts focus variable from the request object to correctly 
+    set the active tabs.'''
     if 'focus' in request.POST.keys():
         return request.POST['focus']
     else: return 'default'
+
 # Create your views here.
 def getbutton(request):
     if 'save' in request.POST.keys():
@@ -264,15 +284,19 @@ def getbutton(request):
 
 def show_messages(request,button,model_name):
     '''provide user feedback on submitting a form.'''
-    if button == 'saveas':messages.warning(request,
-        'saved a copy of '+model_name+'. Use "save" button to store edits to this copy')
-    elif button == 'confirm_delete':messages.success(request, model_name + ' deleted')
+    if button == 'saveas':
+        m = 'saved a copy of '+model_name
+        m += '. Use "save" button to store edits to this copy'
+        messages.warning(request,m)
+    elif button == 'confirm_delete':
+        messages.success(request, model_name + ' deleted')
     elif button == 'cancel':messages.warning(request,'delete aborted')
     elif button == 'error':messages.warning(request,model_name)
     else: messages.success(request, model_name + ' saved')
 
 def close(request):
-    '''page that closes itself for on the fly creation of model instances (loaded in a new tab).'''
+    '''page that closes itself for on the fly creation of model instances 
+    (loaded in a new tab).'''
     return render(request,'utilities/close.html')
 
 def ajax_instance_info(request,identifier,fields = 'all'):
@@ -299,7 +323,8 @@ def edit_protocol(request, app_name, model_name, field_name = None):
     app_name, model_name = app_name.lower(), model_name.lower()
     ProtocolFormSet = modelformset_factory(Protocol, ProtocolForm,
         fields=('field_name','explanation'),extra=1,can_delete=True)
-    queryset=Protocol.objects.filter(app_name = app_name,model_name=model_name)
+    queryset=Protocol.objects.filter(app_name = app_name,
+        model_name=model_name)
     formset = []
     if request.method == 'POST':
         formset = ProtocolFormSet(request.POST,queryset=queryset)
@@ -307,17 +332,17 @@ def edit_protocol(request, app_name, model_name, field_name = None):
             instances = formset.save()
             for instance in instances:
                 need_save = False
-                if not instance.app_name or instance.model_name: need_save = True
+                if not instance.app_name or instance.model_name: 
+                    need_save = True
                 if not instance.app_name: instance.app_name = app_name
                 if not instance.model_name: instance.model_name = model_name
                 if need_save: instance.save()
             print('save is a success')
-            # if not field_name: return render(request,'utilities/close.html')
-            # return HttpResponseRedirect('/'+app_name+'/add_'+ model_name+'/')
             return render(request,'utilities/close.html')
         else: print('not valid',formset.errors,app_name,model_name)
     if not formset: formset = ProtocolFormSet(queryset=queryset)
-    note = 'changes to the protocol text will be visible after refreshing the page'
+    note = 'changes to the protocol text will be visible after '
+    note += 'refreshing the page'
     page_name = 'Protocol ' + model_name
     var = {'formset':formset,'page_name':page_name,'note':note}
     return render(request, 'utilities/add_protocol.html',var)
@@ -337,7 +362,7 @@ def show_edit_screen(request):
 
 def add_info(request):
     n_all_instances = len(get_all_instances())
-    n_incomplete_instances = len(get_instances_without_license_or_reference())
+    n_incomplete_instances=len(get_instances_without_license_or_reference())
     form = None
     if request.method == 'POST':
         form = AddInfoForm(request.POST)
